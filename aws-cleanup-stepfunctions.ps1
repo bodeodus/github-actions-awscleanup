646a32af432eaf99f50749abdc6cfaa4a695440a
bodeodus <111711263+bodeodus@users.noreply.github.com>
2023-05-16 05:43:10 +0100

    Update aws-cleanup-stepfunctions.ps1

--- aws-cleanup-stepfunctions.ps1
+++ aws-cleanup-stepfunctions.ps1
@@ -1,5 +1,5 @@
 #Cleanup state machines
-Get-SFNStateMachineList | % { Remove-SFNStateMachine -StateMachineArn $PSItem.StateMachineArn -WhatIf }
+Get-SFNStateMachineList | % { Remove-SFNStateMachine -StateMachineArn $PSItem.StateMachineArn -Force }
 
 #Cleanup activity tasks
-Get-SFNActivityList | % { Remove-SFNActivity -ActivityArn $PSItem.ActivityArn -WhatIf }
+Get-SFNActivityList | % { Remove-SFNActivity -ActivityArn $PSItem.ActivityArn -Force }
